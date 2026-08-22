0418f5ee33273adecf6667b7e425f1595cac79bf
dpacsiva <dpaksiva@icloud.com>
2026-08-22 13:19:26 +0530

    color added lyrics rename

--- src/components/home/ExploreThemes.tsx
+++ src/components/home/ExploreThemes.tsx
@@ -7,7 +7,7 @@ export function ExploreThemes({ themes }: { themes: Theme[] }) {
   return (
     <FadeInSection as="section" className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
       <div>
-        <SectionLabel>Explore by Theme</SectionLabel>
+        <SectionLabel>Explore Lyrics</SectionLabel>
       </div>
       <div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3">
         {themes.map((theme) => (
